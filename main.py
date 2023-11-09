4a7af1c3e361060b71c5e5b4f5bb36e170ef6669
Krunal Dodiya <kunal.dodiya1@gmail.com>
2023-11-09 09:47:24 +0530

    init

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import uvicorn
-
-from proalgotrader_manager.main import app
-
-app.state = {"host": "127.0.0.1", "port": 5555}
+from proalgotrader_manager.runner import run
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=app.state["host"], port=app.state["port"])
+    run(host="127.0.0.1", port=5555)
