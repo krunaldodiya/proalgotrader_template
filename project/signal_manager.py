1e18fe78762034b119380092593293f00c40705e
Krunal Dodiya <kunal.dodiya1@gmail.com>
2024-03-28 11:42:58 +0530

    done

--- project/signal_manager.py
+++ project/signal_manager.py
@@ -26,9 +26,9 @@ class SignalManager(SignalManagerProtocol):
 
         self.max_daily_loss_percent = self.algorithm.broker_manager.initial_capital * 0.03
 
-        self.max_daily_profit_percent = self.algorithm.broker_manager.initial_capital * 0.10
+        self.max_daily_profit_percent = self.algorithm.broker_manager.initial_capital * 0.15
 
-        self.min_capital_per_lot = 40_000
+        self.min_capital_per_lot = 30_000
 
         number_lots = math.floor(self.algorithm.broker_manager.current_capital / self.min_capital_per_lot)
 
@@ -37,8 +37,8 @@ class SignalManager(SignalManagerProtocol):
         self.tradable_quantities = number_lots * lot_size
 
     @property
-    def supertrend_14(self) -> pd.DataFrame | pd.Series:        
-        return ta.supertrend(
+    def supertrend_14_2(self) -> pd.DataFrame | pd.Series:        
+        data = ta.supertrend(
             high=self.equity_chart.data.high,
             low=self.equity_chart.data.low,
             close=self.equity_chart.data.close,
@@ -46,13 +46,20 @@ class SignalManager(SignalManagerProtocol):
             multiplier=2,
         )
 
-    @property
-    def ema_7(self) -> pd.DataFrame | pd.Series:
-        return ta.ema(close=self.equity_chart.data.close, length=7)
+        return data['SUPERT_14_2.0']
 
     @property
-    def ema_14(self) -> pd.DataFrame | pd.Series:
-        return ta.ema(close=self.equity_chart.data.close, length=14)
+    def supertrend_14_3(self) -> pd.DataFrame | pd.Series:        
+        data = ta.supertrend(
+            high=self.equity_chart.data.high,
+            low=self.equity_chart.data.low,
+            close=self.equity_chart.data.close,
+            length=14,
+            multiplier=3,
+        )
+    
+        return data['SUPERT_14_3.0']
+
 
     @property
     def rsi_14(self) -> pd.DataFrame | pd.Series:
@@ -60,23 +67,25 @@ class SignalManager(SignalManagerProtocol):
 
     @property
     def adx_14(self) -> pd.DataFrame | pd.Series:
-        return ta.adx(
+        data = ta.adx(
             high=self.equity_chart.data.high,
             low=self.equity_chart.data.low,
             close=self.equity_chart.data.close,
             length=14,
         )
+    
+        return data['ADX_14']
 
     @property
     def ce_symbol(self) -> BrokerSymbol:
-        return self.algorithm.add_option(SymbolType.Index.NIFTY, ("weekly", 0), -4, "CE")
+        return self.algorithm.add_option(SymbolType.Index.NIFTY, ("weekly", 0), -2, "CE")
 
     @property
     def pe_symbol(self) -> BrokerSymbol:
-        return self.algorithm.add_option(SymbolType.Index.NIFTY, ("weekly", 0), +4, "PE")
+        return self.algorithm.add_option(SymbolType.Index.NIFTY, ("weekly", 0), +2, "PE")
 
     async def next(self) -> None:
-        between_time = self.algorithm.between_time(time(9, 30), time(15, 15))
+        between_time = self.algorithm.between_time(time(9, 20), time(15, 25))
 
         if not between_time:
             return
@@ -94,57 +103,67 @@ class SignalManager(SignalManagerProtocol):
         if self.algorithm.total_pnl['profit'] >= self.max_daily_profit_percent:
             return
         
-        supertrend_signal = None
+        # supertrend 14, 2 signal started
+        supertrend_14_2_signal = None
 
         if (
-            (self.equity_chart.data.close.iloc[-2] > self.supertrend_14["SUPERT_14_2.0"].iloc[-2]) and (self.equity_chart.data.close.iloc[-1] > self.supertrend_14["SUPERT_14_2.0"].iloc[-1])
+            (self.equity_chart.data.close.iloc[-2] > self.supertrend_14_2.iloc[-2]) and (self.equity_chart.data.close.iloc[-1] > self.supertrend_14_2.iloc[-1])
         ):
-            supertrend_signal = "long"
+            supertrend_14_2_signal = "long"
 
         if (
-            (self.equity_chart.data.close.iloc[-2] < self.supertrend_14["SUPERT_14_2.0"].iloc[-2]) and (self.equity_chart.data.close.iloc[-1] < self.supertrend_14["SUPERT_14_2.0"].iloc[-1])
+            (self.equity_chart.data.close.iloc[-2] < self.supertrend_14_2.iloc[-2]) and (self.equity_chart.data.close.iloc[-1] < self.supertrend_14_2.iloc[-1])
         ):
-            supertrend_signal = "short"
-        
-        ema_signal = None
+            supertrend_14_2_signal = "short"
+        # supertrend 14, 2 signal started
+
+        # supertrend 14, 3 signal started
+        supertrend_14_3_signal = None
 
         if (
-            (self.ema_7.iloc[-2] > self.ema_14.iloc[-2]) and (self.ema_7.iloc[-1] > self.ema_14.iloc[-1])
+            (self.equity_chart.data.close.iloc[-2] > self.supertrend_14_3.iloc[-2]) and (self.equity_chart.data.close.iloc[-1] > self.supertrend_14_3.iloc[-1])
         ):
-            ema_signal = "long"
+            supertrend_14_3_signal = "long"
 
         if (
-            (self.ema_7.iloc[-2] < self.ema_14.iloc[-2]) and (self.ema_7.iloc[-1] < self.ema_14.iloc[-1])
+            (self.equity_chart.data.close.iloc[-2] < self.supertrend_14_3.iloc[-2]) and (self.equity_chart.data.close.iloc[-1] < self.supertrend_14_3.iloc[-1])
         ):
-            ema_signal = "short"
+            supertrend_14_3_signal = "short"
+        # supertrend 14, 3 signal finished
 
+        # rsi 14 signal started
+        rsi_period = (30, 70)
         rsi_signal = False
 
-        if (30 < self.rsi_14.iloc[-2] < 70) and (30 < self.rsi_14.iloc[-1] < 70):
+        if (rsi_period[0] < self.rsi_14.iloc[-2] < rsi_period[1]) and (rsi_period[0] < self.rsi_14.iloc[-1] < rsi_period[1]):
             rsi_signal = True
+        # rsi 14 signal finished
 
+        # adx 14 signal started
+        adx_period = (20, 40)
         adx_signal = False
 
-        if (20 < self.adx_14["ADX_14"].iloc[-2] < 40) and (20 < self.adx_14["ADX_14"].iloc[-1] < 40):
+        if (adx_period[0] < self.adx_14.iloc[-2] < adx_period[1]) and (adx_period[0] < self.adx_14.iloc[-1] < adx_period[1]):
             adx_signal = True
+        # adx 14 signal finished
 
         should_long = (
-            supertrend_signal == "long"
-            and ema_signal == "long"
+            supertrend_14_2_signal == "long"
+            and supertrend_14_3_signal == "long"
             and rsi_signal
             and adx_signal
         )
 
         should_short = (
-            supertrend_signal == "short"
-            and ema_signal == "short"
+            supertrend_14_2_signal == "short"
+            and supertrend_14_3_signal == "short"
             and rsi_signal
             and adx_signal
         )
 
         print("current_datetime", self.algorithm.current_datetime)
-        print("supertrend_signal",supertrend_signal)
-        print("ema_signal",ema_signal)
+        print("supertrend_14_2_signal",supertrend_14_2_signal)
+        print("supertrend_14_3_signal",supertrend_14_3_signal)
         print("rsi_signal",rsi_signal)
         print("adx_signal",adx_signal)
         print("\n")
